c3bf232b8e2dd698c113ce2f6699b2f226ecca55
Mirko Klukas <mklukas@mnheed002.local>
2016-04-06 19:14:02 +0200

    added simple example

---
+++ demo/example.js
@@ -0,0 +1,13 @@
+
+var alphabet = ['a', 'b', 'c', 'd', 'r']
+
+var doc = 'abracadabra';
+var inputSequence = doc.split("")
+var SequenceCoder = require('../lib/LZ78-sequence-coder.js');
+
+var coder  = new SequenceCoder({
+	'alphabet': alphabet,
+	'backShift': 1
+});
+
+coder.learnSequence(inputSequence);
